56c9950b29d233e71bb6a5e2e1d1f3f714c3d723
Simon Zhou <szhou@uber.com>
2017-03-30 20:45:03 -0700

    Introduce error metrics for repair

--- src/java/org/apache/cassandra/tools/NodeProbe.java
+++ src/java/org/apache/cassandra/tools/NodeProbe.java
@@ -987,11 +987,6 @@ public class NodeProbe implements AutoCloseable
         return ssProxy.getTraceProbability();
     }
 
-    public int getExceptionCount()
-    {
-        return (int)StorageMetrics.exceptions.getCount();
-    }
-
     public Map<String, Integer> getDroppedMessages()
     {
         return msProxy.getDroppedMessages();
